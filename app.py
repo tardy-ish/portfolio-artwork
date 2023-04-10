e7202e55b3aef8ef65b0fad2820f7c54b2118a04
tardy-ish <spidey.ds@gmail.com>
2023-04-10 10:43:11 +0530

    Added paintings and completed translations

--- app.py
+++ app.py
@@ -8,21 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<lang>',methods=["POST","GET"])
 def index(lang='en'):
-    art = list(os.listdir('static/assets/artworks'))
-    translations = json.load(open('static/translations.json',encoding='utf-8'))
-    paintingNames = []
-    paintingSizes = []
-    for f in art:
-        s = f[:-4].split('_')
-        paintingNames.append(s[1])
-        paintingSizes.append(s[2])
+    translations = json.load(open('./static/translations.json',encoding='utf-8'))
+    paintings = json.load(open('./static/paintings.json',encoding='utf-8'))
 
     return render_template(
         'artworks.html',
-        files=art,
-        sizes=paintingSizes,
-        names=paintingNames,
         lang=lang,
+        paintings=paintings[lang],
         langP=translations[lang],
     )
 
